5c6ab1ce0cf4d0e67f04d6b4cedea3f2fb49b545
KulkamKakarot <adityamacromaniax@gmail.com>
2026-05-09 12:06:53 +0530

    first commit

---
+++ python/inventory-management/dicts.py
@@ -0,0 +1,75 @@
+"""Functions to keep track and alter inventory."""
+
+
+def create_inventory(items):
+    """Create a dict that tracks the amount (count) of each element on the `items` list.
+
+    :param items: list - list of items to create an inventory from.
+    :return: dict - the inventory dictionary.
+    """
+
+    item_dict = {}
+    
+    for item in items:
+        if item in item_dict:
+            item_dict[item] += 1
+        else:
+            item_dict[item] = 1
+    return item_dict        
+
+
+
+
+def add_items(inventory, items):
+    """Add or increment items in inventory using elements from the items `list`.
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return: dict - the inventory updated with the new items.
+    """
+
+    for item in items:
+        if item in inventory:
+            inventory.update({item: inventory[item] + 1})
+        else:
+            inventory[item] = 1
+    return inventory
+
+def decrement_items(inventory, items):
+    """Decrement items in inventory using elements from the `items` list.
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return: dict - updated inventory with items decremented.
+    """
+
+    for item in items:
+        if item in inventory and inventory[item] > 0:
+            inventory.update({item: inventory[item]-1})
+    return inventory
+
+
+def remove_item(inventory, item):
+    """Remove item from inventory if it matches `item` string.
+
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return: dict - updated inventory with item removed. Current inventory if item does not match.
+    """
+    if item in inventory:
+        inventory.pop(item)
+    return inventory
+
+print(remove_item({"coal":2, "wood":1, "diamond":2}, "coal"))
+
+def list_inventory(inventory):
+    """Create a list containing only available (item_name, item_count > 0) pairs in inventory.
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+    lst = list()
+    for item,value in inventory.items():
+        if inventory[item] != 0:
+            lst.append((item,value))
+    return lst
